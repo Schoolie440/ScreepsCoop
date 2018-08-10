9e9f2fd6005267441de2fd66d195ac50a4d85f88
Ben Schoolcraft <schoolieb44@gmail.com>
2018-08-10 12:57:21 -0400

    Hopefully corrected extension construction. Added blank returns to findNextExtPos when checkSpot comes back false. Only returns good value when checkSpot is true.

--- construct.extensions.js
+++ construct.extensions.js
@@ -3,14 +3,14 @@ utilityFunctions = require('utilityFunctions');
 
 var constructExtensions = {
     run: function(room) {
-        
+        //TODO Remember source in memory, only run find one time
         //find spawns
         var spawners = room.find(FIND_MY_SPAWNS);
-        
+
         var source = spawners[0].pos.findClosestByRange(FIND_SOURCES);
-        
+
         var p = utilityFunctions.findNextExtPos(source);
-        
+
         if(p.x != 0 && p.y != 0) {
             console.log("construct at: " + p.x + " " + p.y)
             room.createConstructionSite(p.x, p.y, STRUCTURE_EXTENSION);
--- utilityFunctions.js
+++ utilityFunctions.js
@@ -1,11 +1,11 @@
 
 var utilityFunctions = {
-    
+
     findNextExtPos: function(source) {
             var spot = source.room.lookAt(Memory.construction.extp.x, Memory.construction.extp.y);
-            
+
             var good = utilityFunctions.checkSpot(spot);
-            
+
             if(!good) {
                 console.log(Memory.construction.extp.x, Memory.construction.extp.y)
                 if(Memory.construction.count < Memory.construction.lap + 2) {
@@ -30,24 +30,28 @@ var utilityFunctions = {
                             break;
                     }
                     console.log(Memory.construction.extp.x, Memory.construction.extp.y)
-                    
+
                     Memory.construction.count++;
-                
+                    return {x: 0, y: 0};
+
                 } else {
-                    
+
                     Memory.construction.count = 0;
                     Memory.construction.dir++;
+
+                    return {x: 0, y: 0};
                 }
+            } else {
+              return Memory.construction.extp;
             }
-        
-        return Memory.construction.extp;
+
     },
 
-    
+
     checkSpot: function(spot) {
-        
+
         var good = true;
-        
+
         for(var i in spot) {
             console.log("Checking spot : ", Memory.construction.extp.x, Memory.construction.extp.y);
             console.log(spot[i].type);
