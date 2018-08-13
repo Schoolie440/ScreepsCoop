7216ba8db3f1a848845352876a5cac155fbfdd9b
schoolie3344 <schoolieb44@gmail.com>
2018-08-13 00:37:00 -0400

    First testable completion of New workerManager system.

--- handler.spawns.js
+++ handler.spawns.js
@@ -1,29 +1,25 @@
 var handlerSpawns = {
     run: function(spawn) {
-        
+
+        //Currently finds all creeps since right now all creeps are worker creeps
         //find all types of creeps for counting purposes
-        var harvesters = _.filter(spawn.room.find(FIND_MY_CREEPS), (creep) => creep.memory.role == 'harvester');
-        var upgraders = _.filter(spawn.room.find(FIND_MY_CREEPS), (creep) => creep.memory.role == 'upgrader');
-        var builders = _.filter(spawn.room.find(FIND_MY_CREEPS), (creep) => creep.memory.role == 'builder');
-        var towerCaddies = _.filter(spawn.room.find(FIND_MY_CREEPS), (creep) => creep.memory.role == 'towerCaddy');
-        var claimers = _.filter(spawn.room.find(FIND_MY_CREEPS), (creep) => creep.memory.role == 'claimer');
-        var miners = _.filter(spawn.room.find(FIND_MY_CREEPS), (creep) => creep.memory.role == 'miners');
-        
+        var workerCreeps = spawn.room.find(FIND_MY_CREEPS);
+
         //determine desired amounts of each body part
         var energyCap = spawn.room.energyCapacityAvailable;
         var fifties = Math.floor(energyCap/50);
         var works = Math.floor(fifties*1/4);
         var carries = Math.floor(fifties*1/4);
         var moves = Math.floor(fifties*1/4);
-        
-        var bodyPartCap = 12;
-        
+
+        var bodyPartCap = 15;
+
         if(works+carries+moves > bodyPartCap) {
             works = carries = moves = bodyPartCap/3;
         }
-        
+
         var bodyParts = [];
-        
+
         //push desired number of each body part to array
         for(var i=0; i<works; i++) {
             bodyParts.push(WORK);
@@ -34,41 +30,22 @@ var handlerSpawns = {
         for(var i=0; i<moves; i++) {
             bodyParts.push(MOVE);
         }
-        
-        var newRole;
+
         var make = false;
-        
+
         //create creep using determined parts/roles
-        if(harvesters.length < 2) {
-            newRole = 'harvester';
-            make = true;
-            
-        }
-        else if(builders.length < 2) {
-            newRole = 'builder';
-            make = true;
-        }
-        else if(upgraders.length < 5) {
-            var newRole = 'upgrader';
-            make = true;
-        }
-        /*else if(miners.length < 4 && spawn.room.find(FIND_STRUCTURES, {filter: (structure) => {return(structure.structureType == STRUCTURE_EXTRACTOR)}}) && spawn.room.find(FIND_MINERALS, {filter: (mineral) => {return(mineral.mineralAmount > 0)}})) {
-            newRole = 'miner';
-            make = true;
-        }*/
-        else if(towerCaddies.length < 1) {
-            newRole = 'towerCaddy';
-            make = true;
-        }
-        
+      if(workerCreeps.length < 10) {
+        make = true;
+      }
+
         //spawn creep, if conditions are correct
-        if(make) { 
-            spawn.createCreep(bodyParts,null,{role: newRole, working: false});
+        if(make) {
+            spawn.createCreep(bodyParts,null,{job: null, target: null, working: false});
         }
         //emergency recovery creeps in case of genocide, prevents minimal energy amounts/
         //no production from halting the colony for extended period
-        if(harvesters == 0) {
-            spawn.createCreep([WORK,CARRY,MOVE], null, {role: 'harvester', working: false});
+        if(workerCreeps.length == 0) {
+            spawn.createCreep([WORK,CARRY,MOVE], null, {job: null, target: null, working: false});
         }
     }
 }
---
+++ jobs.js
@@ -0,0 +1,134 @@
+
+
+var jobs = {
+
+  storeEnergy: function(creep) {
+    if(creep.memory.working && creep.carry.energy == 0) {
+          creep.memory.working = false;
+          creep.memory.job = null;
+    }
+    if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+        creep.memory.working = true;
+    }
+
+    if(creep.memory.working) {
+      if(creep.memory.target == null) {
+        //find nearest non-full depository
+        var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                  filter: (structure) => {
+                      return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
+                          structure.energy < structure.energyCapacity;
+                  }
+        });
+        creep.memory.target = target.id;
+      }
+        //if there is a non-full extension/spawn/tower:
+        if(creep.memory.target != null) {
+            //transfer energy into it
+            if(creep.transfer(Game.getObjectById(creep.memory.target), RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.getObjectById(creep.memory.target));
+            }
+        }
+      }
+      //if not working...
+      else {
+        //find closest source and harvest
+        var source = creep.pos.findClosestByRange(FIND_SOURCES);
+        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(source);
+        }
+      }
+    }
+  },
+
+  buildStructures: function(creep) {
+
+    var target = Game.getObjectById(creep.memory.target);
+
+    if(creep.memory.working && creep.carry.energy == 0) {
+          creep.memory.working = false;
+          creep.memory.job = null;
+          creep.memory.target = null;
+    }
+    if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+        creep.memory.working = true;
+    }
+
+    if(creep.memory.working) {
+      if(creep.build(target) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(target);
+      } else if(creep.build(target) != OK) {
+        creep.memory.working = false;
+        creep.memory.job = null;
+        creep.memory.target = null;
+      }
+    }
+    //creep is not working, i.e. needs energy
+    else {
+      //find closest source and harvest
+      var source = creep.pos.findClosestByRange(FIND_SOURCES);
+      if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source);
+      }
+    }
+  },
+
+  repairStructures: function(creep) {
+
+    if(creep.memory.working && creep.carry.energy == 0) {
+          creep.memory.working = false;
+          creep.memory.target = null;
+          creep.memory.job = null;
+    }
+    if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+        creep.memory.working = true;
+    }
+
+    if(creep.memory.working) {
+      var target = Game.getObjectById(creep.memory.target);
+
+      if(creep.repair(target) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target);
+      }
+
+      if(target.hits == target.hitsMax) {
+          creep.memory.working = false;
+          creep.memory.target = null;
+          creep.memory.job = null;
+      }
+    } else {
+      var source = creep.pos.findClosestByRange(FIND_SOURCES);
+      if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source);
+      }
+    }
+  },
+
+  upgradeController: function(creep) {
+    if(creep.memory.working && creep.carry.energy == 0) {
+        creep.memory.working = false;
+    }
+    if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.working = true;
+    }
+
+    //if working (have full energy)
+    if(creep.memory.working) {
+      //find controller and upgrade it
+        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(creep.room.controller);
+        }
+    }
+    //if we need energy
+    else {
+        //find nearest source and harvest it
+        var source = creep.pos.findClosestByRange(FIND_SOURCES);
+        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(source);
+        }
+    }
+  }
+
+}
+
+module.exports = jobs;
