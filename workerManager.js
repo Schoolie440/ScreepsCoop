70320bd06b368351e2e9ff252b80eb78330e7f13
Ben Schoolcraft <schoolieb44@gmail.com>
2018-08-16 15:17:49 -0400

    completed workerManager commenting and modified caddy and build logic

--- workerManager.js
+++ workerManager.js
@@ -11,6 +11,7 @@ var workerManager = {
       var room = Game.rooms[roomName];
       room.memory.activeEnergy = 0;
       room.memory.activeBuilders = 0;
+      room.memory.activeCaddies = 0;
       room.memory.activeTargets = [];
       room.memory.availableCreeps = [];
     }
@@ -36,6 +37,7 @@ var workerManager = {
         jobs.storeEnergy(creep);
       }
       else if(creep.memory.job == 'caddy') {
+        room.memory.activeCaddies++;
         //call job method to continue creep behavior
         jobs.towerCaddy(creep);
       }
@@ -85,49 +87,68 @@ var workerManager = {
               struct.energy < struct.energyCapacity)
       }});
 
-      
-      if(room.memory.availableCreeps.length > 0 && towers.length > 0) {
+      //if non-full towers are found, and creeps are available
+      if(room.memory.availableCreeps.length > 0 && towers.length > 0 && room.memory.activeCaddies < 1) {
         var newCreep = Game.getObjectById(room.memory.availableCreeps[0]);
+        //assign a creep to bring energy to the tower
         newCreep.memory.job = 'caddy';
+        //call job method to begin creep behavior
         jobs.towerCaddy(newCreep);
+        //remove this creep from the available creeps list
         room.memory.availableCreeps.shift();
       }
 
-
-      if(room.memory.availableCreeps.length > 0 && room.memory.activeBuilders <= 2) {
+      //if there are still creeps available, and fewer than 3 builders are currently active
+      if(room.memory.availableCreeps.length > 0 && room.memory.activeBuilders < 3) {
         var targets = room.find(FIND_CONSTRUCTION_SITES);
-        targets.sort((b,a) => a.hits/a.hitsMax - b.hits/b.hitsMax);
-
-        while(targets.length && room.memory.availableCreeps.length > 0 && room.memory.activeBuilders <= 2) {
+        //sort in descending order (I think)
+        targets.sort((a,b) => b.hits/b.hitsMax - a.hits/a.hitsMax);
+        //runs until out of targets or creeps, or max builders are assigned
+        while(targets.length && room.memory.availableCreeps.length > 0 && room.memory.activeBuilders < 3) {
           var newCreep = Game.getObjectById(room.memory.availableCreeps[0]);
+          //assigns job
           newCreep.memory.job = 'build';
+          //stores build target in creep memory
           newCreep.memory.target = targets[0].id;
+          //call job method to begin creep behavior
           jobs.buildStructures(newCreep);
+          //record the addition of another active builder
           room.memory.activeBuilders += 1;
+          //remove this creep from the available creeps list
           room.memory.availableCreeps.shift();
         }
       }
-
+      //if there are any creeps available
       if(room.memory.availableCreeps.length > 0) {
+        //find repair targets
         targets = room.find(FIND_STRUCTURES, {filter: (struct) => {
                 return (struct.structureType == STRUCTURE_ROAD || struct.structureType == STRUCTURE_CONTAINER) &&
                     (struct.hits < (struct.hitsMax * .75))}});
         //sort weakest to strongest
         targets.sort((a,b) => a.hits/a.hitsMax - b.hits/b.hitsMax);
 
+        //runs until out of targets or available creeps
         while(targets.length > 0 && room.memory.availableCreeps.length > 0) {
           var newCreep = Game.getObjectById(room.memory.availableCreeps[0]);
+          //checks to see if this road is already another creep's target
           if(!room.memory.activeTargets.includes(targets[0].id)) {
+            //assign job to creep
             newCreep.memory.job = 'repair';
+            //store target in creep memory
             newCreep.memory.target = targets[0].id;
+            //call job method to begin creep behavior
             jobs.repairStructures(newCreep);
+            //remove this creep from available creeps
             room.memory.availableCreeps.shift();
+            //add this assigned target to the active targets list
+            room.memory.activeTargets.push(targets[0].id);
           }
+          //remove target from targets list
           targets.shift();
 
         }
       }
-
+      //if any creeps remain available, assign them to upgrade the controller
       if(room.memory.availableCreeps.length > 0) {
         for(var creepID in room.memory.availableCreeps) {
           var newCreep = Game.getObjectById(room.memory.availableCreeps[creepID]);
