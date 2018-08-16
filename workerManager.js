fe6d47a17f5651d9f877d7a8bbeaff8ec21c72d8
Ben Schoolcraft <schoolieb44@gmail.com>
2018-08-16 13:35:02 -0400

    started commenting process

--- workerManager.js
+++ workerManager.js
@@ -23,48 +23,69 @@ var workerManager = {
     for(var name in Game.creeps) {
       var creep = Game.creeps[name];
       if(creep.memory.job == 'store') {
+        //tracks amount of energy that can be stored by each active "store" creep to avoid overassignment
         if(creep.memory.working) {
+          //creep is working, so some of his energy may have already been deposited,
+          //avoid counting twice by counting only what he is carrying
           creep.room.memory.activeEnergy += creep.carry[RESOURCE_ENERGY];
         } else {
+          //count non working creeps by the energy they will eventually deposit
           creep.room.memory.activeEnergy += creep.carryCapacity;
         }
+        //call job method to continue creep behavior
         jobs.storeEnergy(creep);
       }
       else if(creep.memory.job == 'caddy') {
+        //call job method to continue creep behavior
         jobs.towerCaddy(creep);
       }
       else if(creep.memory.job == 'build') {
+        //Tracks number of builders active to prevent construction projects from halting room operation by hogging creeps
         creep.room.memory.activeBuilders++;
+        //call job method to continue creep behavior
         jobs.buildStructures(creep);
       }
       else if(creep.memory.job == 'repair') {
+        //keeps track of targets that are already set for repair to avoid multiple assignment
         creep.room.memory.activeTargets.push(creep.memory.target);
+        //call job method to continue creep behavior
         jobs.repairStructures(creep);
       }
       else if(creep.memory.job == 'upgrade') {
+        //call job method to continue creep behavior
         jobs.upgradeController(creep);
       }
       else if(creep.memory.job == null) {
+        //add creep to available creeps list for this room if he has no assigned job
         creep.room.memory.availableCreeps.push(creep.id);
       }
     }
-
+    //loop through all rooms and find/assign jobs as necessary
     for(var roomName in Game.rooms) {
       var room = Game.rooms[roomName];
+      //finds amount of energy necessary to fill the room's spawn and extensions
       var energyNeeded = room.energyCapacityAvailable-room.energyAvailable-room.memory.activeEnergy;
+      //runs until enough creeps are assigned to fill energy stores or until no more creeps are available
       while(energyNeeded > 0 && room.memory.availableCreeps.length > 0){
+        //next available creep
         var newCreep = Game.getObjectById(room.memory.availableCreeps[0]);
+        //assign the job
         newCreep.memory.job = 'store';
+        //call job method to begin creep behavior
         jobs.storeEnergy(newCreep);
+        //update energy needed with this creeps future contribution
         energyNeeded -= newCreep.carryCapacity;
+        //remove this creep from the available creeps list
         room.memory.availableCreeps.shift();
       }
 
+      //find non-full towers
       var towers = room.find(FIND_STRUCTURES, {filter: (struct) => {
             return (struct.structureType == STRUCTURE_TOWER &&
               struct.energy < struct.energyCapacity)
       }});
 
+      
       if(room.memory.availableCreeps.length > 0 && towers.length > 0) {
         var newCreep = Game.getObjectById(room.memory.availableCreeps[0]);
         newCreep.memory.job = 'caddy';
