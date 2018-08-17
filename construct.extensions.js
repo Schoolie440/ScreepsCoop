465998107f4117f67bfba61a0ee76eec24717341
Brian Schoolcraft <brian.p.schoolcraft@gmail.com>
2018-08-16 23:11:37 -0500

    building source selection logic

--- construct.extensions.js
+++ construct.extensions.js
@@ -8,11 +8,13 @@ var constructExtensions = {
         var controllerLevel = room.controller.level;
         var maxExtensions = CONTROLLER_STRUCTURES['extension'][controllerLevel];  // get max count from constant definitions
 
-        if (room.memory.lastMaxExtensions != maxExtensions) {
+        if (room.memory.lastMaxExtensions != maxExtensions || room.memory.forceExtensions) { // forceExtensions is a manual override for development
 
             var extensions = room.find(FIND_MY_STRUCTURES, {
                 filter: { structureType: STRUCTURE_EXTENSION }
             });
+
+
 
             var extensionCount = extensions.length;
 
@@ -21,11 +23,27 @@ var constructExtensions = {
             }
 
             // find sources, spawns, and controller
-            controllerPos = room.controller.pos;
+            controller = room.controller;
+            spawns = room.spawns;
+            var sources = room.find(FIND_SOURCES_ACTIVE);
+            var spawns = room.find(FIND_MY_SPAWNS);
+            spawns.push(controller);
 
-            console.log(controllerPos);
+            //loop over all spawns / controllers, calculate distances to all spawns
+            for (i=0; i < spawns.length; i++) {
+              var store = spawns[i];
 
-            room.memory.lastMaxExtensions = maxExtensions;
+              for (n=0; n < sources.length; n++) {
+                var spawn = sources[n];
+                console.log(store, spawn);
+              }
+            }
+
+            console.log('spawns', spawns);
+            console.log('sources', sources);
+
+
+            room.memory.lastMaxExtensions = maxExtensions; // store for change detection
         }
     }
 }
