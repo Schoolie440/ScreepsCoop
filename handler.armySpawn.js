5f7e53cfecd84f200b0a7d078443600fe6b96b35
Ben Schoolcraft <schoolieb44@gmail.com>
2018-09-24 11:01:35 -0400

    luyfkghc

--- handler.armySpawn.js
+++ handler.armySpawn.js
@@ -1,46 +1,44 @@
-var handlerArmySpawn = {
-    run: function(spawn, armyCreeps) {
+let handlerArmySpawn = {
+  run: (spawn, armyCreeps) => {
+    //determines desired number of each body part
+    let energyCap = spawn.room.energyCapacityAvailable
+    let hundreds = Math.floor(energyCap / 100)
+    let toughs = Math.floor(((hundreds * 1) / 3) * 2)
+    let attacks = Math.floor((hundreds * 1) / 3)
+    let moves = Math.floor(((hundreds * 1) / 3) * 3)
 
-        //determines desired number of each body part
-        var energyCap = spawn.room.energyCapacityAvailable;
-        var hundreds = Math.floor(energyCap/100);
-        var toughs = Math.floor(hundreds*1/3*2);
-        var attacks = Math.floor(hundreds*1/3);
-        var moves = Math.floor(hundreds*1/3*3);
+    let bodyParts = []
 
-        var bodyParts = [];
-
-        //pushes desired number of each body part into array
-        for(var i=0; i<toughs; i++) {
-            bodyParts.push(TOUGH);
-        }
-        for(var i=0; i<moves; i++) {
-            bodyParts.push(MOVE);
-        }
-        for(var i=0; i<attacks; i++) {
-            bodyParts.push(ATTACK);
-        }
-
-        var newJob;
-        var make = false;
-
-        //construct armyCreeps, little fast ones first, then big ones
-        if(armyCreeps.length < 3) {
-            newJob = 'defender';
-            bodyParts = [MOVE,MOVE,ATTACK,ATTACK]
-            make = true;
-        }
-        else if(armyCreeps.length < 6) {
-            newJob = 'defender';
-            make = true;
-        }
-
-        //spawn armyCreeps, if conditions are correct
-        if(make) {
-          console.log('Spawning Defender');
-          spawn.createCreep(bodyParts,null,{class: 'army', job: newJob});
-        }
+    //pushes desired number of each body part into array
+    for (let i = 0; i < toughs; i++) {
+      bodyParts.push(TOUGH)
     }
+    for (let i = 0; i < moves; i++) {
+      bodyParts.push(MOVE)
+    }
+    for (let i = 0; i < attacks; i++) {
+      bodyParts.push(ATTACK)
+    }
+
+    let newOrder
+    let make = false
+
+    //construct armyCreeps, little fast one first, then big ones
+    if (armyCreeps.length < 1) {
+      newOrder = 'defender'
+      bodyParts = [MOVE, MOVE, ATTACK, ATTACK]
+      make = true
+    } else if (armyCreeps.length < 6) {
+      newOrder = 'defender'
+      make = true
+    }
+
+    //spawn armyCreeps, if conditions are correct
+    if (make) {
+      console.log('Spawning Defender')
+      spawn.createCreep(bodyParts, null, { class: 'army', order: newOrder })
+    }
+  },
 }
 
-module.exports = handlerArmySpawn;
+module.exports = handlerArmySpawn
